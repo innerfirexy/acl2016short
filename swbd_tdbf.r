c32e72609e746c950f636747a1fd0d2eb652cdf4
Yang Xu <innerfirexy@gmail.com>
2016-02-08 16:03:43 -0500

    include rds for plotting

--- swbd_tdbf.r
+++ swbd_tdbf.r
@@ -2,39 +2,65 @@
 # Yang Xu
 # 2/8/2016
 
-library(RMySQL)
 library(lme4)
 library(data.table)
 library(dplyr)
+library(ggplot2)
 
-# ssh yvx5085@brain.ist.psu.edu -i ~/.ssh/id_rsa -L 1234:localhost:3306
-conn = dbConnect(MySQL(), host = '127.0.0.1', user = 'yang', port = 1234, password = "05012014", dbname = 'swbd')
-sql = 'SELECT convID, turnID, speaker, localID, globalID, wordNum, ent, treeDepth, branchingFactor,
-    tileID, inTileID, tileStarter FROM entropy WHERE globalID <= 100'
-df = dbGetQuery(conn, sql)
-
+# read from RDS
+df = readRDS('swbd.leader.ent.merged.rds')
 df = rename(df, td = treeDepth, bf = branchingFactor)
-dt = data.table(df)
 
-# add role column: 'initiator', 'responder'
-dt$role = 'responder'
-dt[dt$speaker == dt$tileStarter,]$role = 'initiator'
-dt$role = as.factor(dt$role)
+df$role = 'initiator'
+df[!df$byLeader,]$role = 'responder'
 
 # td, bf vs. globalID
-summary(lmer(td ~ globalID + (1|convID), dt)) # t = 6.4
-summary(lmer(bf ~ globalID + (1|convID), dt)) # t = -7
+summary(lmer(td ~ globalID + (1|convID), df)) # t = 5.6
+summary(lmer(bf ~ globalID + (1|convID), df)) # t = -6
 
-# td, bf vs. inTileID
-summary(lmer(td ~ inTileID + (1|convID) + (1|tileID), subset(dt, role == 'initiator')))
-summary(lmer(td ~ inTileID + (1|convID) + (1|tileID), subset(dt, role == 'responder')))
+# td, bf vs. inTopicID
+summary(lmer(td ~ inTopicID + (1|convID) + (1|topicID), subset(df, role == 'initiator'))) # t = -0.4
+summary(lmer(td ~ inTopicID + (1|convID) + (1|topicID), subset(df, role == 'responder'))) # t = 6.6
 
-summary(lmer(bf ~ inTileID + (1|convID) + (1|tileID), subset(dt, role == 'initiator')))
-summary(lmer(bf ~ inTileID + (1|convID) + (1|tileID), subset(dt, role == 'responder')))
+summary(lmer(bf ~ inTopicID + (1|convID) + (1|topicID), subset(df, role == 'initiator'))) # t = 2.7
+summary(lmer(bf ~ inTopicID + (1|convID) + (1|topicID), subset(df, role == 'responder'))) # t = -11
 
-# wordNum vs. inTileID
-summary(lmer(wordNum ~ inTileID + (1|convID) + (1|tileID), subset(dt, role == 'initiator'))) # t = -9.63
-summary(lmer(wordNum ~ inTileID + (1|convID) + (1|tileID), subset(dt, role == 'responder'))) # t = -16.54
+# wordNum vs. inTopicID
+summary(lmer(wordNum ~ inTopicID + (1|convID) + (1|topicID), subset(df, role == 'initiator'))) # t = -36.46
+summary(lmer(wordNum ~ inTopicID + (1|convID) + (1|topicID), subset(df, role == 'responder'))) # t = 22.69
 
-# ent vs. inTileID
-summary(lmer(ent ~ inTileID + (1|convID) + (1|tileID), subset(dt, role == 'initiator')))
+
+## examine the distr of td, bf among all speakers
+dt = data.table(df)
+setkey(dt, convID, speaker)
+
+# add speakerID column
+dt_spkrId = unique(dt[, .(convID, speaker)])
+dt_spkrId$speakerID = 1:nrow(dt_spkrId)
+dt = dt[dt_spkrId]
+
+setkey(dt, speakerID)
+dt_td = dt[, .(td = mean(td)), by = speakerID]
+dt_bf = dt[, .(bf = mean(td)), by = speakerID]
+
+distr_td = ggplot(dt_td, aes(td)) + geom_histogram()
+plot(distr_td)
+
+distr_bf = ggplot(dt_bf, aes(bf)) + geom_histogram()
+plot(distr_bf)
+
+# differentiate two speakers by whether td, bf are above or below mean values
+dt_td$tdLvl = 'high'
+dt_td[dt_td$td < mean(dt_td$td),]$tdLvl = 'low'
+dt = dt[dt_td[,.(speakerID, tdLvl)]]
+
+summary(lmer(td ~ globalID + (1|speakerID), subset(dt, tdLvl == 'high'))) # t = 2.4
+summary(lmer(td ~ globalID + (1|speakerID), subset(dt, tdLvl == 'low'))) # t = 5.2
+summary(lmer(td ~ inTopicID + (1|speakerID), subset(dt, tdLvl == 'high'))) # t = 3.8
+summary(lmer(td ~ inTopicID + (1|speakerID), subset(dt, tdLvl == 'low'))) # t = 1.9
+
+# p_td = ggplot(subset(dt, inTopicID <= 10),
+#         aes(x = inTopicID, y = td, color = tdLvl, fill = tdLvl, lty = tdLvl)) +
+#     stat_summary(fun.data = mean_cl_boot, geom = 'ribbon', alpha = .5) +
+#     stat_summary(fun.y = mean, geom = 'line')
+# plot(p_td)
