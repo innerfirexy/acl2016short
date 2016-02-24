c913d2e00f771fc19c2cbebd8a4ef5aff537d53d
Yang Xu <innerfirexy@gmail.com>
2016-02-24 12:52:03 -0500

    done with swbd_tdbf.py

--- swbd_tdbf.py
+++ swbd_tdbf.py
@@ -19,6 +19,55 @@ def db_conn(db_name):
                     db = db_name)
     return conn
 
+# compute td and bf columns
+def compute_tdbf():
+    conn = db_conn('swbd')
+    cur = conn.cursor()
+    # select keys and parsed from table
+    sql = 'SELECT convID, globalID, parsed FROM entropy'
+    cur.execute(sql)
+    data = cur.fetchall()
+    # initialize
+    pool = Pool(multiprocessing.cpu_count())
+    manager = Manager()
+    queue = manager.Queue()
+    # mp
+    args = [(d, queue) for d in data]
+    result = pool.map_async(compute_tdbf_worker, args, chunksize=5000)
+    # manager loop
+    while True:
+        if result.ready():
+            print('\n all rows processed')
+            break
+        else:
+            sys.stdout.write('\r{}/{} processed'.format(queue.qsize(), len(args)))
+            sys.stdout.flush()
+            time.sleep(1)
+    # update
+    processed_results = result.get()
+    for i, res in enumerate(processed_results):
+        c_id, g_id, td, bf = res
+        sql = 'UPDATE entropy SET td = %s, bf = %s WHERE convID = %s AND globalID = %s'
+        cur.execute(sql, (td, bf, c_id, g_id))
+        if i % 999 == 0 and i > 0:
+            sys.stdout.write('\r{}/{} updated'.format(i+1, len(processed_results)))
+            sys.stdout.flush()
+    conn.commit()
+
+# worker func for compute_tdbf
+def compute_tdbf_worker(args):
+    (c_id, g_id, parsed_str), queue = args
+    # construct the full tree, and get sub_tree
+    full_tree = Tree.fromstring(parsed_str)
+    sub_tree = full_tree[0] if full_tree.label() == 'ROOT' else full_tree
+    # compute td and bf
+    td = sub_tree.height()
+    bf = numpy.mean([len(t) for t in sub_tree.subtrees()])
+    # capsulate and return
+    queue.put(1)
+    return (c_id, g_id, td, bf)
+
+
 # main
 if __name__ == '__main__':
-    
+    compute_tdbf()
